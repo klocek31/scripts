e79955c3a6f0ff57b71444e1798888cb3a1d4d47
klocek31 <kacperotulak75@gmail.com>
2024-06-12 21:08:17 +0200

    im too lazy to finish it tbh

---
+++ functions.py
@@ -0,0 +1,8 @@
+# this shit just calculates various forms of quadratic functions, cause too lazy to do it by myself
+# in polish because fuck you thats why
+
+a = "Wpisz a (ta liczba przy x^2)"
+b = "Wpisz b (liczba przy zwykłym x)"
+c = "wyraz wolny czy coś takiego"
+
+
